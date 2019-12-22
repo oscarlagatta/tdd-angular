a0505d8aa2ebbb993049a58ffdceb11d275c9b9b
Oscar Lagatta <oscarlagatta@gmail.com>
2019-12-22 16:22:26 +0000

    scaffolding dialog with material desgin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomesComponent } from './components/homes/homes.component';
 
+// material design dialog
+import { MatDialogModule } from '@angular/material/dialog';
+import { BookComponent } from './components/book/book.component'
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomesComponent
+    HomesComponent,
+    BookComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/homes/homes.component.ts
+++ src/app/components/homes/homes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { of } from "rxjs";
 import { DataService } from 'src/app/services/data.service';
 import { DialogService } from 'src/app/services/dialog.service';
+import { BookComponent } from '../book/book.component';
 
 @Component({
   selector: "app-homes",
@@ -18,26 +19,13 @@ export class HomesComponent implements OnInit {
 
     this.homes$ = this.dataService.getHomes$();
 
-    // this.homes$ = of([
-    //   {
-    //     title: "home 1",
-    //     imageUrl: "assets/listing.jpg",
-    //     location: "new york"
-    //   },
-    //   {
-    //     title: "home 2",
-    //     imageUrl: "assets/listing.jpg",
-    //     location: "boston"
-    //   },
-    //   {
-    //     title: "home 3",
-    //     imageUrl: "assets/listing.jpg",
-    //     location: "london"
-    //   }
-    // ]);
+
   }
 
   openDialog() {
-    this.dialogService.open();
+    this.dialogService.open(BookComponent, {
+      width: '250px',
+      data: {}
+    });
   }
 }
